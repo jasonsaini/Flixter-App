29cdb25553c6a5db5c71c4ead3dd343c015d8dd8
jasonsaini <jasonsaini.exe@gmail.com>
2022-10-02 19:25:01 -0400

    Created basic class for Movie items

--- app/src/main/java/com/example/flixter/Movie.kt
+++ app/src/main/java/com/example/flixter/Movie.kt
@@ -1,4 +1,17 @@
 package com.example.flixter
 
-class Movie {
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class Movie(
+    val movieId: Int,
+    val averageVote: Double,
+    val posterImageUrl: String,
+    val title: String,
+    val overview: String,
+   ) : Parcelable{
+
+
 }
