e918d4f744ce8165db0ba47c7d6e5dc059fe267f
jasonsaini <jasonsaini.exe@gmail.com>
2022-10-02 19:30:10 -0400

    Added class functionality for JSON parsing

--- app/src/main/java/com/example/flixter/Movie.kt
+++ app/src/main/java/com/example/flixter/Movie.kt
@@ -1,8 +1,9 @@
 package com.example.flixter
 
 import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
-
+import org.json.JSONArray
 
 @Parcelize
 data class Movie(
@@ -12,6 +13,26 @@ data class Movie(
     val title: String,
     val overview: String,
    ) : Parcelable{
+    @IgnoredOnParcel
+    val posterImageUrlPath = "https://image.tmdb.org/t/p/w342/$posterImageUrl"
+    companion object{
+        fun fromJSONArray(movieJSONArray: JSONArray): List<Movie>{
+            val movies = mutableListOf<Movie>()
+            for(i in 0 until movieJSONArray.length()) {
+                val movieJSON = movieJSONArray.getJSONObject(i)
+                movies.add(
+                    Movie(
+                        movieJSON.getInt("id"),
+                        movieJSON.getDouble("vote_average"),
+                        movieJSON.getString("poster_path"),
+                        movieJSON.getString("title"),
+                        movieJSON.getString("overview")
+                    )
+                )
+            }
+            return movies;
+        }
+    }
 
-
+    }
 }
