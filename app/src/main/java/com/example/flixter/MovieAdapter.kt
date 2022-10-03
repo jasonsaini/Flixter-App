c8eac6a10ce2781704bf7276c161d005a35282b5
jasonsaini <jasonsaini.exe@gmail.com>
2022-10-02 22:41:04 -0400

    Cleaned up code on Movie Adapter

--- app/src/main/java/com/example/flixter/MovieAdapter.kt
+++ app/src/main/java/com/example/flixter/MovieAdapter.kt
@@ -13,21 +13,17 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
-
-
-const val MOVIE_EXTRA = "MOVIE_EXTRA"
-private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        Log.i(TAG, "onCreateViewHolder")
+        Log.i("MovieAdapter", "onCreateViewHolder")
         val view = LayoutInflater.from(context).inflate(R.layout.movie_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Log.i(TAG, "onBindViewholder position $position")
+        Log.i("MovieAdapter", "onBindViewholder position $position")
         val movie = movies[position]
         holder.bind(movie)
     }
@@ -55,9 +51,9 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
         override fun onClick(v: View?) {
             val movie = movies[adapterPosition] // Tells us what the position of the particular viewHolder
-/
+
             val intent = Intent(context, DetailActivity::class.java)
-            intent.putExtra(MOVIE_EXTRA, movie)
+            intent.putExtra("MOVIE_EXTRA", movie)
             context.startActivity(intent)
         }
     }
